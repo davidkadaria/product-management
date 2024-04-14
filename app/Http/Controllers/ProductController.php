8bd29c8cf55d70e5d3e0dd4d5db9733f214707e1
DavidKadaria <david.qada@gmail.com>
2024-04-14 20:34:30 +0400

    Generate ProductController & add index function that renders Home page and passes all products to it

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+
+        return Inertia::render('Home', [
+            'products' => $products,
+        ]);
+    }
+}
