022521fd80954c7ad8cbd3db11c2102f9ac6fda0
DavidKadaria <david.qada@gmail.com>
2024-04-15 00:36:52 +0400

    Modify routes. Use get() instead of resources(). Besides, add a new endpoint for the dahsboard page

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 
-Route::resource('/', ProductController::class);
+Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/dashboard', [ProductController::class, 'dashboard'])->name('dashboard');
