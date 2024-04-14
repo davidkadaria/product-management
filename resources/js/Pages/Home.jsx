09297a97d066a3ca80acb833855ecf96b364bbca
DavidKadaria <david.qada@gmail.com>
2024-04-15 01:41:47 +0400

    Modify Home page. Add basic table with pagination

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,107 @@
-function Home() {
+import { Table, Flex, Button, Typography } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+
+function Home({ products }) {
+    const columns = [
+        {
+            title: "Image",
+            dataIndex: "images",
+            key: "image",
+
+            render: (images) => {
+                try {
+                    images = JSON.parse(images);
+
+                    if (images.length > 0) {
+                        return (
+                            <img
+                                src={images[0]}
+                                alt={"product image"}
+                                style={{ width: "50px" }}
+                            />
+                        );
+                    }
+                } catch (error) {
+                    console.log("error", error);
+                }
+
+                return null;
+            },
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Description",
+            dataIndex: "description",
+            key: "description",
+            render: (description) => {
+                if (description.length > 30) {
+                    return description.substring(0, 30).trim() + "...";
+                }
+
+                return description;
+            },
+        },
+        {
+            title: "Price",
+            dataIndex: "price",
+            key: "price",
+            render: (price) => `$${price}`,
+        },
+        {
+            title: "Categories",
+            dataIndex: "categories",
+            key: "categories",
+            render: (categories) => {
+                try {
+                    if (categories.length > 0) {
+                        const categoriesAsStr = categories
+                            .map((category) => category.name)
+                            .join(", ");
+                        return categoriesAsStr;
+                    }
+                } catch (error) {
+                    console.log("error", error);
+                }
+
+                return null;
+            },
+        },
+        {
+            title: "Action",
+            key: "action",
+            render: (text, record) => (
+                <Flex gap="small">
+                    <Button type="primary">View Details</Button>
+                    <Button type="primary" danger icon={<DeleteOutlined />}>
+                        Delete
+                    </Button>
+                </Flex>
+            ),
+        },
+    ];
+
     return (
         <div>
-            <h1>Home</h1>
+            <Title>Products List</Title>
+            <Table
+                dataSource={products.data.map((product) => ({
+                    key: product.id,
+                    ...product,
+                }))}
+                columns={columns}
+                pagination={{
+                    pageSize: products.per_page,
+                    total: products.total,
+                    current: products.current_page,
+                }}
+                onChange={(pagination) => console.log("pagination", pagination)}
+            />
         </div>
     );
 }
