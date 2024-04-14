803b3310d29eb22cccc9b8e3c50dd37ade5f8e39
DavidKadaria <david.qada@gmail.com>
2024-04-15 01:46:36 +0400

    Remove column structuring logic from the Home page and use the getProductListTableColumns util method instead

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,91 +1,9 @@
-import { Table, Flex, Button, Typography } from "antd";
-import { DeleteOutlined } from "@ant-design/icons";
+import { Table, Typography } from "antd";
+import { getProductListTableColumns } from "../utils";
 
 const { Title } = Typography;
 
 function Home({ products }) {
-    const columns = [
-        {
-            title: "Image",
-            dataIndex: "images",
-            key: "image",
-
-            render: (images) => {
-                try {
-                    images = JSON.parse(images);
-
-                    if (images.length > 0) {
-                        return (
-                            <img
-                                src={images[0]}
-                                alt={"product image"}
-                                style={{ width: "50px" }}
-                            />
-                        );
-                    }
-                } catch (error) {
-                    console.log("error", error);
-                }
-
-                return null;
-            },
-        },
-        {
-            title: "Name",
-            dataIndex: "name",
-            key: "name",
-        },
-        {
-            title: "Description",
-            dataIndex: "description",
-            key: "description",
-            render: (description) => {
-                if (description.length > 30) {
-                    return description.substring(0, 30).trim() + "...";
-                }
-
-                return description;
-            },
-        },
-        {
-            title: "Price",
-            dataIndex: "price",
-            key: "price",
-            render: (price) => `$${price}`,
-        },
-        {
-            title: "Categories",
-            dataIndex: "categories",
-            key: "categories",
-            render: (categories) => {
-                try {
-                    if (categories.length > 0) {
-                        const categoriesAsStr = categories
-                            .map((category) => category.name)
-                            .join(", ");
-                        return categoriesAsStr;
-                    }
-                } catch (error) {
-                    console.log("error", error);
-                }
-
-                return null;
-            },
-        },
-        {
-            title: "Action",
-            key: "action",
-            render: (text, record) => (
-                <Flex gap="small">
-                    <Button type="primary">View Details</Button>
-                    <Button type="primary" danger icon={<DeleteOutlined />}>
-                        Delete
-                    </Button>
-                </Flex>
-            ),
-        },
-    ];
-
     return (
         <div>
             <Title>Products List</Title>
@@ -94,7 +12,7 @@ function Home({ products }) {
                     key: product.id,
                     ...product,
                 }))}
-                columns={columns}
+                columns={getProductListTableColumns()}
                 pagination={{
                     pageSize: products.per_page,
                     total: products.total,
