0f88e4b0ffd7efd816c9a12d36d927b015f03ab1
DavidKadaria <david.qada@gmail.com>
2024-04-15 00:25:21 +0400

    Modify Product module. Add useFactory flag

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Seed the categories first
+        $this->call(CategorySeeder::class);
+        // Seed the products
+        $this->call(ProductSeeder::class);
 
         User::factory()->create([
             'name' => 'Test User',
