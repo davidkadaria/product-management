faa6523c2f843696ada6dc07328494ce41b45e69
DavidKadaria <david.qada@gmail.com>
2024-04-16 22:59:29 +0400

    Modify Home page: add layout

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,45 +1,52 @@
 import { Inertia } from "@inertiajs/inertia";
 import { Link } from "@inertiajs/inertia-react";
-import { router } from "@inertiajs/react";
-import { Flex, Table, Typography, Button } from "antd";
+import { Flex, Table, Typography, Button, Layout } from "antd";
 import { DashboardOutlined } from "@ant-design/icons";
 import { getProductListTableColumns } from "../utils";
+
+const { Header, Content } = Layout;
 
 const { Title } = Typography;
 
 function Home({ products }) {
     return (
-        <div>
-            <Flex justify="space-between" align="center">
-                <Title>Products List</Title>
+        <Layout>
+            <Header
+                style={{ backgroundColor: "transparent", marginBottom: 25 }}
+            >
+                <Flex justify="space-between" align="center">
+                    <Title>Products List</Title>
 
-                <Link href={"/dashboard"}>
-                    <Button icon={<DashboardOutlined />} type="primary">
-                        Dashboard
-                    </Button>
-                </Link>
-            </Flex>
-            <Table
-                dataSource={products.data.map((product) => ({
-                    key: product.id,
-                    ...product,
-                }))}
-                columns={getProductListTableColumns()}
-                pagination={{
-                    pageSize: products.per_page,
-                    total: products.total,
-                    current: products.current_page,
-                }}
-                onChange={(pagination) => {
-                    Inertia.reload({
-                        data: {
-                            page: pagination.current,
-                            per_page: pagination.pageSize,
-                        },
-                    });
-                }}
-            />
-        </div>
+                    <Link href={"/dashboard"}>
+                        <Button icon={<DashboardOutlined />} type="primary">
+                            Dashboard
+                        </Button>
+                    </Link>
+                </Flex>
+            </Header>
+            <Content>
+                <Table
+                    dataSource={products.data.map((product) => ({
+                        key: product.id,
+                        ...product,
+                    }))}
+                    columns={getProductListTableColumns()}
+                    pagination={{
+                        pageSize: products.per_page,
+                        total: products.total,
+                        current: products.current_page,
+                    }}
+                    onChange={(pagination) => {
+                        Inertia.reload({
+                            data: {
+                                page: pagination.current,
+                                per_page: pagination.pageSize,
+                            },
+                        });
+                    }}
+                />
+            </Content>
+        </Layout>
     );
 }
 
