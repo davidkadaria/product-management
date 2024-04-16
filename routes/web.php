88604ba04f8c6f6b72fa0013f2011b3452e5767f
DavidKadaria <david.qada@gmail.com>
2024-04-16 06:48:37 +0400

    Modify routers root: add delete and details routes

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,7 @@ use App\Http\Controllers\ProductController;
 
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::delete('/', [ProductController::class, 'index'])->name('home');
+Route::get('/products/{id}', [ProductController::class, 'productDetail'])->name('product.detail');
+Route::delete('/products/{id}', [ProductController::class, 'deleteProduct'])->name('product.delete');
 Route::get('/dashboard', [ProductController::class, 'dashboard'])->name('dashboard');
