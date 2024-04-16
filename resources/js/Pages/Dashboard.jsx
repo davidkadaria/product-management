dd3c70245479fb6f636068b8a98a1df3933fff21
DavidKadaria <david.qada@gmail.com>
2024-04-17 02:20:52 +0400

    Update Dahsboard page content

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,8 +1,35 @@
+// import { Inertia } from "@inertiajs/inertia";
+import { Link } from "@inertiajs/inertia-react";
+import { Flex, Typography, Button, Layout, Tabs } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+import { getDashboardTabItems } from "../utils";
+
+const { Header, Content } = Layout;
+
+const { Title } = Typography;
+
 function Dashboard() {
+    const items = getDashboardTabItems();
+
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <Layout>
+            <Header
+                style={{ backgroundColor: "transparent", marginBottom: 25 }}
+            >
+                <Flex justify="space-between" align="center">
+                    <Title>Dashboard</Title>
+
+                    <Link href={"/"}>
+                        <Button icon={<HomeOutlined />} type="primary">
+                            Home
+                        </Button>
+                    </Link>
+                </Flex>
+            </Header>
+            <Content>
+                <Tabs defaultActiveKey={items[0].key} items={items} centered />
+            </Content>
+        </Layout>
     );
 }
 
