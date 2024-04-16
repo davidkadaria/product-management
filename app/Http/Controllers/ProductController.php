8b8b65aed9d1626f573e3e065c4b1a5d31d486da
DavidKadaria <david.qada@gmail.com>
2024-04-16 06:49:09 +0400

    Modify ProductController: add controller methods for delete and detail endpoints

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,6 +22,25 @@ class ProductController extends Controller
         ]);
     }
 
+    public function productDetail($id)
+    {
+        $product = Product::with('categories')->find($id);
+
+        return Inertia::render('ProductDetail', [
+            'product' => $product,
+        ]);
+    }
+
+    public function deleteProduct($id)
+    {
+        $product = Product::find($id);
+
+        $product->delete();
+
+        // return success message
+        return redirect()->back()->with('success', 'Product deleted successfully');
+    }
+
     public function dashboard()
     {
 
