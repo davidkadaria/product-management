7e18e82b803142263a1cd98f8b26cd8b44da49d9
DavidKadaria <david.qada@gmail.com>
2024-04-17 16:54:44 +0400

    Modify root router file: add form endpoints

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+
 
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
@@ -9,3 +11,6 @@ Route::delete('/', [ProductController::class, 'index'])->name('home');
 Route::get('/products/{id}', [ProductController::class, 'productDetail'])->name('product.detail');
 Route::delete('/products/{id}', [ProductController::class, 'deleteProduct'])->name('product.delete');
 Route::get('/dashboard', [ProductController::class, 'dashboard'])->name('dashboard');
+
+Route::post('/create-product', [ProductController::class, 'createProduct'])->name('product.create');
+Route::post('/create-category', [CategoryController::class, 'create'])->name('category.create');
